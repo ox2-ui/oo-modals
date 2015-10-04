bbe5ad68ae1fb279dcc3861f410b08ecd34fb71c
Lauricio Su <me@lauricio.com>
2015-10-04 19:35:15 +0700

    updated less dependency to 2.5.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ox2:modals',
-  summary: 'Overlay modals',
-  version: '1.0.0',
+  summary: 'TESTING_DO_NOT_USE Overlay modals',
+  version: '1.1.0',
   git: ' /* Fill me in! */ '
 });
 
@@ -10,7 +10,7 @@ var C = 'client';
 var CS = [C, S];
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.2.1');
+  api.versionsFrom('1.2.0.2');
   api.export('ooModalOpen');
   api.export('ooModalClose');
   api.export('ooModalScrollOpen');
@@ -21,7 +21,7 @@ Package.onUse(function(api) {
     ]);
   // 3rd party
   api.use([
-    'lauricio:less-autoprefixer@1.0.7','mquandalle:jade@0.4.1'
+    'lauricio:less-autoprefixer@2.5.0_3','mquandalle:jade@0.4.1'
     ]);
   api.addFiles('lib/oo-modals.jade', C);
   api.addFiles('lib/oo-modals.js', C);
