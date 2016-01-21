8508f5aa39f8853d2d493f4103fdf2bcdcb2c41c
Lauricio Su <me@lauricio.com>
2016-01-21 16:31:09 +0700

    version bump

--- package.js
+++ package.js
@@ -1,8 +1,13 @@
 Package.describe({
   name: 'ox2:modals',
-  summary: 'TESTING_DO_NOT_USE Overlay modals',
-  version: '1.1.0',
-  git: ' /* Fill me in! */ '
+  version: '2.0.0',
+  // Brief, one-line summary of the package.
+  summary: 'DO NOT USE',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: ''
 });
 
 var S = 'server';
