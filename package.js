d23c7c00d32d27d4ea43f68b06189d7cbaf47261
Lauricio Su <me@lauricio.com>
2016-06-18 21:34:15 +0700

    added webkit blur to backdrops

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ox2:modals',
-  version: '2.4.0',
+  version: '2.5.0',
   // Brief, one-line summary of the package.
   summary: 'DO NOT USE',
   // URL to the Git repository containing the source code for this package.
